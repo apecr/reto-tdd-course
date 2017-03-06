cbe1fc7d5c8b0caf2387e8e42db1c7b484193928
apecr <albertoeyo@outlook.es>
2017-03-06 12:26:02 +0100

    Incluimos nuevos tests
    Nuevos tests (hasta el 62)

--- src/test/java/com/eyo/roman/numerals/service/RomanNumeralsServiceTest.java
+++ src/test/java/com/eyo/roman/numerals/service/RomanNumeralsServiceTest.java
@@ -112,4 +112,92 @@ public class RomanNumeralsServiceTest {
         assertEquals( "X", romanNumeral );
     }
     
+    @Test
+    public void whenNumber11ReturnXI(){
+        //When
+        String romanNumeral = this.romanNumeralsService.convertDecimalNumberToRomanNumeral( 11 );
+        //Then
+        assertEquals( "XI", romanNumeral );
+    }
+    
+    @Test
+    public void whenNumber15ReturnXV(){
+        //When
+        String romanNumeral = this.romanNumeralsService.convertDecimalNumberToRomanNumeral( 15 );
+        //Then
+        assertEquals( "XV", romanNumeral );
+    }
+    
+    @Test
+    public void whenNumber18ReturnXVIII(){
+        //When
+        String romanNumeral = this.romanNumeralsService.convertDecimalNumberToRomanNumeral( 18 );
+        //Then
+        assertEquals( "XVIII", romanNumeral );
+    }
+    
+    @Test
+    public void whenNumber19ReturnXIX(){
+        //When
+        String romanNumeral = this.romanNumeralsService.convertDecimalNumberToRomanNumeral( 19 );
+        //Then
+        assertEquals( "XIX", romanNumeral );
+    }
+    
+    @Test
+    public void whenNumber21ReturnXX1(){
+        //When
+        String romanNumeral = this.romanNumeralsService.convertDecimalNumberToRomanNumeral( 21 );
+        //Then
+        assertEquals( "XXI", romanNumeral );
+    }
+    
+    @Test
+    public void whenNumber27ReturnXXVII(){
+        //When
+        String romanNumeral = this.romanNumeralsService.convertDecimalNumberToRomanNumeral( 27 );
+        //Then
+        assertEquals( "XXVII", romanNumeral );
+    }
+    
+    @Test
+    public void whenNumber32ReturnXXXII(){
+        //When
+        String romanNumeral = this.romanNumeralsService.convertDecimalNumberToRomanNumeral( 32 );
+        //Then
+        assertEquals( "XXXII", romanNumeral );
+    }
+    
+    @Test
+    public void whenNumber45ReturnXLV(){
+        //When
+        String romanNumeral = this.romanNumeralsService.convertDecimalNumberToRomanNumeral( 45 );
+        //Then
+        assertEquals( "XLV", romanNumeral );
+    }
+    
+    @Test
+    public void whenNumber50ReturnL(){
+        //When
+        String romanNumeral = this.romanNumeralsService.convertDecimalNumberToRomanNumeral( 50 );
+        //Then
+        assertEquals( "L", romanNumeral );
+    }
+    
+    @Test
+    public void whenNumber51ReturnLI(){
+        //When
+        String romanNumeral = this.romanNumeralsService.convertDecimalNumberToRomanNumeral( 51 );
+        //Then
+        assertEquals( "LI", romanNumeral );
+    }
+    
+    @Test
+    public void whenNumber62ReturnLXII(){
+        //When
+        String romanNumeral = this.romanNumeralsService.convertDecimalNumberToRomanNumeral( 62 );
+        //Then
+        assertEquals( "LXII", romanNumeral );
+    }
+    
 }
